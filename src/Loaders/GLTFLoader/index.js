6d8ebed2d6ca6130dde697f81114b3d749a5ecbb
pensiveant <142905570003@qq.com>
2024-11-28 14:27:04 +0800

    feat: GLTFLoader 示例

---
+++ src/Loaders/GLTFLoader/index.js
@@ -0,0 +1,59 @@
+// 顶点颜色
+import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+//
+const loader = new GLTFLoader();
+loader.load(
+  // resource URL
+  "./SU7.glb",
+  // called when the resource is loaded
+  function (gltf) {
+    scene.add(gltf.scene);
+
+    // gltf.animations; // Array<THREE.AnimationClip>
+    // gltf.scene; // THREE.Group
+    // gltf.scenes; // Array<THREE.Group>
+    // gltf.cameras; // Array<THREE.Camera>
+    // gltf.asset; // Object
+  },
+  function (xhr) {
+    console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
+  },
+  // called when loading has errors
+  function (error) {
+    console.log("An error happened");
+  }
+);
+
+camera.position.y = 25;
+
+// 创建轨道控制器
+const controls = new OrbitControls(camera, renderer.domElement);
+// 动态渲染
+function animation() {
+  controls.update();
+  renderer.render(scene, camera);
+  requestAnimationFrame(animation);
+}
+animation();
+
+const gridHelper = new THREE.GridHelper(20, 20);
+gridHelper.material.transparent = true;
+gridHelper.material.opacity = 0.5;
+scene.add(gridHelper);
+
+const axesHelper = new THREE.AxesHelper(5);
+scene.add(axesHelper);
