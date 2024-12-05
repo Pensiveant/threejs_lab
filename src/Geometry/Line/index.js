4158563043ef4554e326599dd8e996a255a57b1d
pensiveant <142905570003@qq.com>
2024-12-05 16:19:32 +0800

    feat：Line示例

---
+++ src/Geometry/Line/index.js
@@ -0,0 +1,58 @@
+import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+camera.position.z = 25;
+
+// 创建Line
+const points = [];
+points.push( new THREE.Vector3( 0, 0, 0 ) );
+points.push( new THREE.Vector3( 2, 0, 0 ) );
+points.push( new THREE.Vector3( 2, 2, 0 ) );
+points.push( new THREE.Vector3( 0, 2, 0 ) );
+const geometry = new THREE.BufferGeometry().setFromPoints(points);
+
+
+const material = new THREE.LineBasicMaterial({
+  color: "rgb(255,0,0)",
+});
+const mesh = new THREE.Line(geometry, material);
+scene.add(mesh);
+mesh.position.set(-5, 0, 0);
+
+// 
+const mesh2 = new THREE.LineLoop(geometry, material);
+scene.add(mesh2);
+
+// LineSegments
+const mesh1 = new THREE.LineSegments(geometry, material);
+scene.add(mesh1);
+mesh1.position.set(5, 0, 0);
+
+// 创建轨道控制器
+const controls = new OrbitControls(camera, renderer.domElement);
+// 动态渲染
+function animation() {
+  controls.update();
+  renderer.render(scene, camera);
+  requestAnimationFrame(animation);
+}
+animation();
+
+// const axesHelper = new THREE.AxesHelper(5);
+// scene.add(axesHelper);
+
+// 创建网格辅助工具
+const gridHelper = new THREE.GridHelper(20, 20, 0xffffff, 0xffffff);
+gridHelper.material.transparent = true; // 为了更方便观察, 设置opacity透明度
+gridHelper.material.opacity = 0.5;
+scene.add(gridHelper);
